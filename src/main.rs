feab9f65052ca0235fcf02f237a27af1975a7f33
Sebastian <sebastian.schein@yahoo.com>
2024-03-17 20:11:06 +0000

    database set up and integrated in CI/CD

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use zero2prod::run;
+use std::net::TcpListener;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind to 8000 port");
+    run(listener)?.await
 }
---
+++ src/routes/mod.rs
@@ -0,0 +1,5 @@
+mod health_check;
+mod subscriptions;
+
+pub use self::health_check::*;
+pub use self::subscriptions::*;
